f66e259a6aa09c0b65631feb1539c07e789ab611
Peti0127 <83165013+Peti0127@users.noreply.github.com>
2026-08-24 11:19:14 +0200

    Add files via upload

--- Resources/taskpane.js
+++ Resources/taskpane.js
@@ -1352,7 +1352,7 @@ setDefaultButton.addEventListener("click", async () => {
 });
 openSignatureSettingsButton.addEventListener("click", () => {
   const signatureId = contextSignatureId || "standard";
-  window.location.href = `taskpane.html?view=settings&signature=${encodeURIComponent(signatureId)}&v=0.8.14`;
+  window.location.href = `taskpane.html?view=settings&signature=${encodeURIComponent(signatureId)}&v=0.8.15`;
 });
 editCustomButton.addEventListener("click", () => {
   const item = customSignatures.items.find((entry) => entry.id === contextSignatureId);
